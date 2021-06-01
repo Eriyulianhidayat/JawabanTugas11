6c785ddaa9d961511dd467f19fd7d8f4aa3dfb2f
Eriyulianhidayat <85097802+Eriyulianhidayat@users.noreply.github.com>
2021-06-01 12:07:51 +0700

    Update daftar kendaraan dan cari.php

--- daftar kendaraan dan cari.php
+++ daftar kendaraan dan cari.php
@@ -35,6 +35,8 @@ else{
 <head>
 	<title>daftar kendaraan</title>
 	<link rel="stylesheet" type="text/css" href="gaya.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 	<form action="" method="post">
